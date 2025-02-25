dbdb8c2e0198e4cab3cf930d4795e0f8231be17d
ditmfv <ditmfvv@gmail.com>
2025-02-25 00:38:36 -0800

    1

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -10,17 +10,17 @@ export const renderPhotoCards = photoData => {
         comments,
         downloads,
       }) =>
-        <a href="${largeImageURL}" class="gallery-item">
-  <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="360" />
-  <div class="photo-info">
-    <p class="photo-info-item">Likes<span>${likes}</span></p>
- <p class="photo-info-item">Views<span>${views}</span></p>
- <p class="photo-info-item">Comments<span>${comments}</span></p>
- <p class="photo-info-item">Downloads<span>${downloads}</span></p>
-  </div>
-  </div>
-  </a>
+        `<a href="${largeImageURL}" class="gallery-item">
+          <div class="photo-card">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" width="360" />
+            <div class="photo-info">
+              <p class="photo-info-item">Likes<span>${likes}</span></p>
+              <p class="photo-info-item">Views<span>${views}</span></p>
+              <p class="photo-info-item">Comments<span>${comments}</span></p>
+              <p class="photo-info-item">Downloads<span>${downloads}</span></p>
+            </div>
+          </div>
+        </a>`
     )
     .join('');
 };
